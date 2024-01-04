4c88230c9000feac46afedb21438a7374c170b93
JKrafty <kraft.jackson@gmail.com>
2024-01-04 11:49:24 -0600

    Added Explosive Barrel
    Learned about class URadialForceComponent and applying impulse.
    
    Applied mesh and material to ExplosiveBarrel_BP.
    
    Solution to Assignment 1.

---
+++ Source/ActionRoguelike_/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SExplosiveBarrel.h"
+#include "Components/StaticMeshComponent.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	MeshComp->SetSimulatePhysics(true);
+	RootComponent = MeshComp;
+
+	ForceComp = CreateDefaultSubobject<URadialForceComponent>("ForceComp");
+	ForceComp->SetupAttachment(MeshComp);
+
+	// Leaving this on applies small constant force via component "tick" (optional)
+	ForceComp->SetAutoActivate(false);
+
+	ForceComp->Radius = 750.0f;
+	ForceComp->ImpulseStrength = 2500.0f; // Alt: 20000.0 if bImpulseVelChange = false
+	// Optional, ignores 'Mass' of other objects (if false, the impulse strength will be much higher to push most objects depending on Mass)
+	ForceComp->bImpulseVelChange = true;
+
+	// Optional, default constructor of component already adds 4 object types to affect, excluding WorldDynamic
+	ForceComp->AddCollisionChannelToAffect(ECC_WorldDynamic);
+
+	// Binding either in constructor or in PostInitializeComponent() below
+	// MeshComp->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnActorHit);
+
+}
+
+// Called when the game starts or when spawned
+void ASExplosiveBarrel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASExplosiveBarrel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASExplosiveBarrel::PostInitializeComponents()
+{
+	// Don't forget to call the parent
+	Super::PostInitializeComponents();
+
+	MeshComp->OnComponentHit.AddDynamic(this, &ASExplosiveBarrel::OnActorHit);
+
+}
+
+void ASExplosiveBarrel::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	ForceComp->FireImpulse();
+
+}
---
+++ Source/ActionRoguelike_/Public/SExplosiveBarrel.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SExplosiveBarrel.generated.h"
+
+class UStaticMeshComponent;
+class URadialForceComponent;
+
+UCLASS()
+class ACTIONROGUELIKE__API ASExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASExplosiveBarrel();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere)
+	URadialForceComponent* ForceComp;
+
+	virtual void PostInitializeComponents() override;
+
+	UFUNCTION() // Allows exposure to Unreal's property system, letting the engine bind the function properly.
+	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
